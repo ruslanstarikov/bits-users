54d9c56c0c0230cd4ee73b7dedbd4e18acda6f54
Ruslan Starikov <ruslan@beef-burger.com>
2023-12-12 10:43:16 +1100

    Error variable

--- src/views/components/basic/text.blade.php
+++ src/views/components/basic/text.blade.php
@@ -3,7 +3,7 @@
     <div class="mt-2">
         <input value="{{ $value ?? '' }}" id="{{ $name }}" name="{{ $name }}" type="text" autocomplete="{{ $autocomplete ?? '' }}" required class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6">
     </div>
-    @error($name)
+    @error($$name)
     <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
     @enderror
 </div>
