cb8a89cf48f0966135e61d841862034a32840c94
Ruslan Starikov <ruslan@beef-burger.com>
2023-12-12 10:58:21 +1100

    More issues

--- src/views/components/basic/email.blade.php
+++ src/views/components/basic/email.blade.php
@@ -3,5 +3,5 @@
     <div class="mt-2">
         <input value="{{ $value ?? '' }}" id="{{ $name }}" name="{{ $name }}" type="email" autocomplete="{{ $autocomplete ?? '' }}" required class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6">
     </div>
-    @include('user-registration::components.basic.error', ['name' => $name])
+    @include('vendor.user-registration.components.basic.error', ['name' => $name])
 </div>
