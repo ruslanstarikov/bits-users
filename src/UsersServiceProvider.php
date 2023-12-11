867869364c89975d25008d536775e7aa96740332
Ruslan Starikov <ruslan@beef-burger.com>
2023-12-11 18:46:54 +1100

    Publish views

--- src/UsersServiceProvider.php
+++ src/UsersServiceProvider.php
@@ -8,6 +8,11 @@ class UsersServiceProvider extends ServiceProvider
 	{
 		$this->loadRoutesFrom(__DIR__.'/routes.php');
 		$this->loadViewsFrom(__DIR__.'/views', 'user-registration');
+		if ($this->app->runningInConsole()) {
+			$this->publishes([
+				__DIR__.'/views' => resource_path('views/vendor/user-registration'),
+			], 'user-registration-views');
+		}
 	}
 
 	public function register()
