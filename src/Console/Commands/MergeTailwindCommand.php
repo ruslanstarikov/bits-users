22bda887ed10d3635038d2553f9047873d61f39d
Ruslan Starikov <ruslan@beef-burger.com>
2023-12-12 12:29:15 +1100

    Merge tailwind stuff

---
+++ src/Console/Commands/MergeTailwindCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace LaravelHtmx\UserRegistration\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class MergeTailwindCommand extends Command
+{
+    protected $signature = 'bits-laravel-htmx:merge-tailwind';
+
+    protected $description = 'Will add necessary plugins to the tailwind installation and packages.json';
+
+    public function handle()
+    {
+		$packageTailwindConfig = __DIR__.'/configs/tailwind.config.js';
+		$packagePackageJson = __DIR__.'/configs/package.json';
+
+		$userTailwindConfig = base_path('tailwind.config.js');
+		$userPackageJson = base_path('package.json');
+
+		$mergedTailwindConfig = array_merge(
+			require($packageTailwindConfig),
+			require($userTailwindConfig)
+		);
+		$mergedPackageJson = array_merge(
+			json_decode(File::get($packagePackageJson), true),
+			json_decode(File::get($userPackageJson), true)
+		);
+		File::put($userTailwindConfig, 'module.exports = '.var_export($mergedTailwindConfig, true).";\n");
+		File::put($userPackageJson, json_encode($mergedPackageJson, JSON_PRETTY_PRINT));
+
+		$this->info('Tailwind CSS and package.json files merged successfully.');
+	}
+}
--- src/UsersServiceProvider.php
+++ src/UsersServiceProvider.php
@@ -2,12 +2,17 @@
 
 namespace LaravelHtmx\UserRegistration;
 use Illuminate\Support\ServiceProvider;
+use LaravelHtmx\UserRegistration\Console\Commands\MergeTailwindCommand;
+
 class UsersServiceProvider extends ServiceProvider
 {
 	public function boot()
 	{
 		$this->loadRoutesFrom(__DIR__.'/routes.php');
 		$this->loadViewsFrom(__DIR__.'/views', 'user-registration');
+		$this->commands([
+			MergeTailwindCommand::class,
+		]);
 		if ($this->app->runningInConsole()) {
 			$this->publishes([
 				__DIR__.'/views' => resource_path('views/vendor/user-registration'),
