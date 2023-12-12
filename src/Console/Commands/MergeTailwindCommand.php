579b88216c4114eb276bf8e3456c0ccc5217e2cf
Ruslan Starikov <ruslan@beef-burger.com>
2023-12-12 13:16:55 +1100

    Merge tailwind config try out

--- src/Console/Commands/MergeTailwindCommand.php
+++ src/Console/Commands/MergeTailwindCommand.php
@@ -19,17 +19,40 @@ class MergeTailwindCommand extends Command
 		$userTailwindConfig = base_path('tailwind.config.js');
 		$userPackageJson = base_path('package.json');
 
-		$mergedTailwindConfig = array_merge(
-			require($packageTailwindConfig),
-			require($userTailwindConfig)
-		);
+		$this->mergePackageJson($packagePackageJson, $userPackageJson);
+
+		$this->info('Tailwind CSS and package.json files merged successfully.');
+	}
+
+	private function mergePackageJson(string $packagePackageJson, string $userPackageJson)
+	{
 		$mergedPackageJson = array_merge(
 			json_decode(File::get($packagePackageJson), true),
 			json_decode(File::get($userPackageJson), true)
 		);
-		File::put($userTailwindConfig, 'module.exports = '.var_export($mergedTailwindConfig, true).";\n");
 		File::put($userPackageJson, json_encode($mergedPackageJson, JSON_PRETTY_PRINT));
+	}
 
-		$this->info('Tailwind CSS and package.json files merged successfully.');
+	private function mergeTailwindConfig(string $packageTailwindConfigPath, string $userTailwindConfigPath)
+	{
+		$pluginsToAdd = [
+			'@tailwindcss/forms',
+			'@tailwindcss/typography',
+			'@tailwindcss/aspect-ratio'
+		];
+		if (file_exists($userTailwindConfigPath)) {
+			$userTailwindConfig = file_get_contents($userTailwindConfigPath);
+			$contents = preg_replace('/^module.exports = /', '', $userTailwindConfig);
+			$contents = rtrim(rtrim($contents, "\n"), ';');
+			$config = json_decode($contents, true);
+			if (!isset($config['plugins'])) {
+				// If "plugins" key doesn't exist, add it with the plugins array
+				$config['plugins'] = $pluginsToAdd;
+			} else {
+				// If "plugins" key exists, merge the desired plugins with the existing ones
+				$config['plugins'] = array_merge($pluginsToAdd, $config['plugins']);
+			}
+			$this->info(json_encode($config, JSON_PRETTY_PRINT));
+		}
 	}
 }
--- src/configs/tailwind.config.js
+++ src/configs/tailwind.config.js
@@ -1,7 +1,3 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-
-/** @type {import('tailwindcss').Config} */
 export default {
     plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio')],
 };
