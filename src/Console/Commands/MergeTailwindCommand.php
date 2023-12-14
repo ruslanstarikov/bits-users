ae5eaa974ced41b8a9b2e0a53968bbfae3f38b52
Ruslan Starikov <ruslan@beef-burger.com>
2023-12-14 20:46:19 +1100

    Publishing assets are more verbose

--- src/Console/Commands/MergeTailwindCommand.php
+++ src/Console/Commands/MergeTailwindCommand.php
@@ -13,33 +13,36 @@ class MergeTailwindCommand extends Command
 
     public function handle()
     {
-		$packageTailwindConfig = __DIR__.'/../../configs/tailwind.config.js';
-		$packagePackageJson = __DIR__.'/../../configs/package.json';
+		$sourceTailwindConfig = __DIR__.'/../../configs/tailwind.config.js';
+		$sourcePackageJsonConfig = __DIR__.'/../../configs/package.json';
+		$sourcePostCssConfig = __DIR__.'/../../configs/postcss.config.js';
 
-		$userTailwindConfig = base_path('tailwind.config.js');
-		$userPackageJson = base_path('package.json');
+		$targetTailwind = base_path('tailwind.config.js');
+		$targetPackageJson = base_path('package.json');
+		$targetPostCssConfig = base_path('postcss.config.js');
+		$this->mergePackageJson($sourcePackageJsonConfig, $targetPackageJson);
 
-		$this->mergePackageJson($packagePackageJson, $userPackageJson);
-		if(!file_exists($userTailwindConfig)) {
-			File::copy($packageTailwindConfig, $userTailwindConfig);
-		}
-		else
-		{
-			$replaceTailwindQuestion = $this->ask('Do you want to replace your tailwind.config.js file? (y/n)');
-			if($replaceTailwindQuestion == 'y')
-			{
-				File::copy($packageTailwindConfig, $userTailwindConfig);
-			}
-			else
-			{
-				$this->info("You will need to manually add the following plugins to your tailwind.conf.js");
-				$this->info("[require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio')]");
-			}
-		}
+		$tailwindInstruction = "You will need to manually add the following plugins to your tailwind.conf.js\n[require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio')]";
 
+		$this->mergeFilePrompt($sourceTailwindConfig, $targetTailwind, $tailwindInstruction);
+		$this->mergeFilePrompt($sourcePostCssConfig, $targetPostCssConfig);
 		$this->info("Configuration complete. Now run npm install && npm run dev");
 	}
 
+	private function mergeFilePrompt(string $sourceFile, string $targetFile, string $instruction = null)
+	{
+		if(!file_exists($targetFile)) {
+			File::copy($sourceFile, $targetFile);
+		}
+		else
+		{
+			$replaceFileQuestion = $this->ask('Do you want to replace your '.$targetFile.' file? (y/n)');
+			if($replaceFileQuestion == 'y')
+			{
+				File::copy($sourceFile, $targetFile);
+			}
+		}
+	}
 	private function mergePackageJson(string $libPackageJsonFilePath, string $userPackageJsonFilePath)
 	{
 		$userPackageArray =  json_decode(File::get($userPackageJsonFilePath), true);
