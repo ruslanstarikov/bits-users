2b80fa6189bc7759aaa9b12cb86509c091adc106
Ruslan Starikov <ruslan@beef-burger.com>
2023-12-14 21:56:40 +1100

    Port Request

--- src/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ src/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -3,12 +3,12 @@
 namespace LaravelHtmx\UserRegistration\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use LaravelHtmx\UserRegistration\Http\Requests\Auth\LoginRequest;
 
 class AuthenticatedSessionController extends Controller
 {
