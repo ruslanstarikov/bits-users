77354fff98e5006b6f0f971417f8e12d5e2e41a1
Ruslan Starikov <ruslan@beef-burger.com>
2023-12-14 21:48:06 +1100

    Password field fix

--- src/views/components/basic/password.blade.php
+++ src/views/components/basic/password.blade.php
@@ -5,10 +5,8 @@
         <div class="text-sm">
             <a href="{{ route('password.request') }}" class="font-semibold text-indigo-400 hover:text-indigo-300">Forgot password?</a>
         </div>
-        @error($name)
-        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-        @enderror
         @endif
+        @include('vendor.user-registration.components.basic.error', ['name' => $name])
     </div>
     <div class="mt-2">
         <input id="{{ $name }}" name="{{ $name }}" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6">
